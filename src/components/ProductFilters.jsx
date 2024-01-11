906c804a8228ca05c63a72f1caa403f72d5b8f8b
Gedaias Jair <gedaias@geda.dev>
2024-01-10 22:59:38 -0600

    Create the elements to filter and order the products

---
+++ src/components/ProductFilters.jsx
@@ -0,0 +1,136 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+export default function ProductFilters({
+  handleOrder,
+  handleCategorySelection,
+  handleRatingSelection,
+}) {
+  const [minPrice, setMinPrice] = useState(0);
+
+  const handleMinPrice = (event) => {
+    setMinPrice(event.target.value);
+  };
+
+  return (
+    <>
+      <label htmlFor="order-products" hidden>
+        Order by
+      </label>
+      <select name="order-products" id="order-products" onChange={handleOrder}>
+        <option value="">Order by:</option>
+        <option value="low-price">Price low to high</option>
+        <option value="high-price">Price high to low</option>
+        <option value="rating">Best ratings</option>
+      </select>
+      <div className="filters">
+        <h3>Filters:</h3>
+        <div className="price-filter">
+          <label htmlFor="price-filter">Minimum price</label>
+          <input
+            type="range"
+            name="price-filter"
+            id="price-filter"
+            min="0"
+            max="900"
+            value={minPrice}
+            onChange={handleMinPrice}
+          />
+          <span>{minPrice}</span>
+        </div>
+        <fieldset>
+          <legend>By category:</legend>
+          <div className="category-option">
+            <input
+              type="checkbox"
+              name="category-filter"
+              id="electronics"
+              value="electronics"
+              onChange={handleCategorySelection}
+            />
+            <label htmlFor="electronics">Electronics</label>
+          </div>
+          <div className="category-option">
+            <input
+              type="checkbox"
+              name="category-filter"
+              id="jewelry"
+              value="jewelery"
+              onChange={handleCategorySelection}
+            />
+            <label htmlFor="electronics">Jewelry</label>
+          </div>
+          <div className="category-option">
+            <input
+              type="checkbox"
+              name="category-filter"
+              id="men-clothing"
+              value="men's clothing"
+              onChange={handleCategorySelection}
+            />
+            <label htmlFor="electronics">Men&apos;s clothing</label>
+          </div>
+          <div className="category-option">
+            <input
+              type="checkbox"
+              name="category-filter"
+              id="women-clothing"
+              value="women's clothing"
+              onChange={handleCategorySelection}
+            />
+            <label htmlFor="electronics">Women&apos;s clothing</label>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>By rating:</legend>
+          <div className="rating-option">
+            <input
+              type="checkbox"
+              name="rating-filter"
+              id="five-stars"
+              value="5"
+              onChange={handleRatingSelection}
+            />
+            <label htmlFor="five-stars">{"⭐".repeat(5)}</label>
+          </div>
+          <div className="rating-option">
+            <input
+              type="checkbox"
+              name="rating-filter"
+              id="four-stars"
+              value="4"
+              onChange={handleRatingSelection}
+            />
+            <label htmlFor="five-stars">{"⭐".repeat(4)}</label>
+          </div>
+          <div className="rating-option">
+            <input
+              type="checkbox"
+              name="rating-filter"
+              id="three-stars"
+              value="3"
+              onChange={handleRatingSelection}
+            />
+            <label htmlFor="five-stars">{"⭐".repeat(3)}</label>
+          </div>
+          <div className="rating-option">
+            <input
+              type="checkbox"
+              name="rating-filter"
+              id="two-stars"
+              value="2"
+              onChange={handleRatingSelection}
+            />
+            <label htmlFor="five-stars">{"⭐".repeat(2)}</label>
+          </div>
+        </fieldset>
+      </div>
+    </>
+  );
+}
+
+ProductFilters.propTypes = {
+  handleOrder: PropTypes.func,
+  handleCategorySelection: PropTypes.func,
+  handleRatingSelection: PropTypes.func,
+};
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+import ProductFilters from "./ProductFilters";
 import "../styles/products.css";
 
 export default function Products() {
@@ -11,9 +12,27 @@ export default function Products() {
       .then((json) => setProductsList(json));
   }, [setProductsList, productsList]);
 
+  const handleOrder = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handleCategorySelection = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handleRatingSelection = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div className="products-section">
-      <div className="product-filters"></div>
+      <div className="product-filters">
+        <ProductFilters
+          handleOrder={handleOrder}
+          handleCategorySelection={handleCategorySelection}
+          handleRatingSelection={handleRatingSelection}
+        />
+      </div>
       <div className="products-container">
         {productsList.map((product) => (
           <ProductCard key={product.id} product={product} />
