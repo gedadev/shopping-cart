0f8356377360d567d99a225597ee8a9a302b673d
Gedaias Jair <gedaias@geda.dev>
2024-01-26 21:44:05 -0600

    Create the requests
    Fetch the products and get the 5 best rated
    Fetch the categories

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,8 @@ import fashionBanner from "../assets/casual-fashion-banner.png";
 import saleBanner from "../assets/sale-banner.png";
 import shippingBanner from "../assets/free-shipping-banner.png";
 import "../styles/home.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Home() {
   const images = [
@@ -11,6 +13,21 @@ export default function Home() {
     { original: saleBanner },
     { original: shippingBanner },
   ];
+
+  const [topProducts, setTopProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        const sorted = json.sort((a, b) => b.rating.rate - a.rating.rate);
+        setTopProducts(sorted.slice(0, 5));
+      });
+    fetch("https://fakestoreapi.com/products/categories")
+      .then((res) => res.json())
+      .then((json) => setCategories(json));
+  }, []);
 
   return (
     <div className="home-section">
@@ -20,8 +37,8 @@ export default function Home() {
         showBullets={true}
         showFullscreenButton={false}
       />
-      <div className="top-products"></div>
-      <div className="categories"></div>
+      <div className="top-products">{console.log(topProducts)}</div>
+      <div className="categories">{console.log(categories)}</div>
     </div>
   );
 }
