60ab54dea73694258beb5715f9eba630a1964657
Gedaias Jair <gedaias@geda.dev>
2024-01-29 20:19:27 -0600

    Create state to handle the input data

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,39 @@
+import { useState } from "react";
 import "../styles/contact.css";
 
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <div className="contact-section">
       <h2>Contact us</h2>
-      <form className="form-container">
+      <form className="form-container" onChange={handleInput}>
         <div className="form-item">
           <label htmlFor="name">Your name:</label>
-          <input type="text" name="name" id="name" />
+          <input type="text" name="name" id="name" value={formData.name} />
         </div>
         <div className="form-item">
           <label htmlFor="email">Your email:</label>
-          <input type="email" name="email" id="email" />
+          <input type="email" name="email" id="email" value={formData.email} />
         </div>
         <div className="form-item">
           <label htmlFor="message">Your message:</label>
-          <textarea name="message" id="message" cols="30" rows="6"></textarea>
+          <textarea
+            name="message"
+            id="message"
+            cols="30"
+            rows="6"
+            value={formData.message}
+          />
         </div>
         <button type="submit" className="send-message-button">
           Send
