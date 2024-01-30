6f51405036d5be29a35c54642d97c7e8eb08399b
Gedaias Jair <gedaias@geda.dev>
2024-01-29 22:46:16 -0600

    Create a footer and the styles
    Adjust the other pages styles to include the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./styles/App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import ContextProvider from "./ContextProvider";
 
@@ -9,6 +10,7 @@ function App() {
       <ContextProvider>
         <Header />
         <Outlet />
+        <Footer />
       </ContextProvider>
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "../styles/footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <ul>
+        <li className="footer-link">Conditions of use</li>
+        <li className="footer-link">Privacy Notice</li>
+        <li className="footer-link">Sitemap</li>
+      </ul>
+      <p>2024 &#169; Gedashop - All rights reserved</p>
+    </div>
+  );
+}
