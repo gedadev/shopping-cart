980a4626844be901cc154e0f4f54a5129753c617
Gedaias Jair <gedaias@geda.dev>
2024-01-29 20:42:54 -0600

    Create a confirmation view and create a state to handle it

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "../styles/contact.css";
+import { Link } from "react-router-dom";
 
 export default function Contact() {
   const [formData, setFormData] = useState({
@@ -7,38 +8,73 @@ export default function Contact() {
     email: "",
     message: "",
   });
+  const [sentConfirmation, setSentConfirmation] = useState(false);
 
   const handleInput = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(
+      `New message\nfrom: ${e.target[0].value}, email: ${e.target[1].value}\nMessage: ${e.target[2].value}`
+    );
+    setSentConfirmation(true);
+  };
+
   return (
     <div className="contact-section">
-      <h2>Contact us</h2>
-      <form className="form-container" onChange={handleInput}>
-        <div className="form-item">
-          <label htmlFor="name">Your name:</label>
-          <input type="text" name="name" id="name" value={formData.name} />
+      {!sentConfirmation ? (
+        <>
+          <h2>Contact us</h2>
+          <form className="form-container" onSubmit={handleSubmit}>
+            <div className="form-item">
+              <label htmlFor="name">Your name:</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={formData.name}
+                onChange={handleInput}
+              />
+            </div>
+            <div className="form-item">
+              <label htmlFor="email">Your email:</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={formData.email}
+                onChange={handleInput}
+              />
+            </div>
+            <div className="form-item">
+              <label htmlFor="message">Your message:</label>
+              <textarea
+                name="message"
+                id="message"
+                cols="30"
+                rows="6"
+                value={formData.message}
+                onChange={handleInput}
+              />
+            </div>
+            <button type="submit" className="send-message-button">
+              Send
+            </button>
+          </form>
+        </>
+      ) : (
+        <div className="sent-confirmation-container">
+          <p>Thanks for your message, we will get in touch soon!</p>
+          <Link to="/">
+            <button type="button" className="return-button">
+              Go back to home
+            </button>
+          </Link>
         </div>
-        <div className="form-item">
-          <label htmlFor="email">Your email:</label>
-          <input type="email" name="email" id="email" value={formData.email} />
-        </div>
-        <div className="form-item">
-          <label htmlFor="message">Your message:</label>
-          <textarea
-            name="message"
-            id="message"
-            cols="30"
-            rows="6"
-            value={formData.message}
-          />
-        </div>
-        <button type="submit" className="send-message-button">
-          Send
-        </button>
-      </form>
+      )}
     </div>
   );
 }
