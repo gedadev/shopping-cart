43107cd81c9b86b60a5235f06ee23cedcec25afc
Gedaias Jair <gedaias@geda.dev>
2024-01-29 20:09:26 -0600

    Create the contact section and the styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,26 @@
+import "../styles/contact.css";
+
 export default function Contact() {
   return (
-    <>
-      <p>contact us</p>
-    </>
+    <div className="contact-section">
+      <h2>Contact us</h2>
+      <form className="form-container">
+        <div className="form-item">
+          <label htmlFor="name">Your name:</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div className="form-item">
+          <label htmlFor="email">Your email:</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div className="form-item">
+          <label htmlFor="message">Your message:</label>
+          <textarea name="message" id="message" cols="30" rows="6"></textarea>
+        </div>
+        <button type="submit" className="send-message-button">
+          Send
+        </button>
+      </form>
+    </div>
   );
 }
